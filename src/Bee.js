19e8913da481b9938219fcb41d830c1ca9e45ad2
Yang Zhou <yzhou3991@gmail.com>
2018-04-30 11:17:53 -0700

    inherit bee from grub

--- src/Bee.js
+++ src/Bee.js
@@ -1,8 +1,9 @@
 var Bee = function() {
+  Grub.call(this);
   this.age = 5;
   this.color = 'yellow';
   this.food = 'jelly';
   this.job = 'keep on growing';
 };
-
-Bee.prototype.eat = function(){};
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
