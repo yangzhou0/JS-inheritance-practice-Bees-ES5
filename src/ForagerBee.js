7ecf297e06effdadc4f60e27b0eead42f22e859e
Yang Zhou <yzhou3991@gmail.com>
2018-04-30 11:06:59 -0700

    set up foragerbee class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+
+  this.age = 10;
+  this.color = 'yellow';
+  this.food = 'jelly';
+  this.job = 'find pollen';
+  this.honeyPot = 0;
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype.eat = function(){};
+ForagerBee.prototype.forage = function(item){
+  this.treasureChest.push(item);
+};
