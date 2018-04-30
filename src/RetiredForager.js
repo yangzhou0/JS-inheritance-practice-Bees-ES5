ed22408cbebff72dd926dcf214cb57bd8a94291b
Yang Zhou <yzhou3991@gmail.com>
2018-04-30 11:11:39 -0700

    finish retiredforagebee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,17 @@
 var RetiredForagerBee = function() {
+  this.age = 40;
+  this.color = 'grey';
+  this.food = 'jelly';
+  this.job = 'gamble';
+  this.canFly = false;
+  this.treasureChest = [];
 };
 
+RetiredForagerBee.prototype.eat = function(){};
+RetiredForagerBee.prototype.forage = function(){
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(){
+  this.treasureChest.push('treature');
+}
